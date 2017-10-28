b2a087eebdbfbe6486a07ce7a55023f6f9e053bc
Eva <evajohnson714@gmail.com>
2017-10-28 16:42:57 +0300

    Removing all duplicates in a list

---
+++ duplicates.py
@@ -0,0 +1,17 @@
+#Write a function remove_duplicates that takes in a list and removes elements of the list that are the same.
+def remove_duplicates(inputlist):
+    if inputlist == []:
+        return []
+    
+# Sort the input list from low to high    
+    inputlist = sorted(inputlist)
+# Initialize the output list, and give it the first value of the now-sorted input list
+    outputlist = [inputlist[0]]
+
+# Go through the values of the sorted list and append to the output list
+# ...any values that are greater than the last value of the output list
+    for i in inputlist:
+        if i > outputlist[-1]:
+            outputlist.append(i)
+        
+    return outputlist
