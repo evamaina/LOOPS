626da5596aab3413a20ccf0e5205c11950c109d0
Eva <evajohnson714@gmail.com>
2017-11-30 14:38:41 +0300

    calculate average and sum

--- gradepr.py
+++ gradepr.py
@@ -1,5 +1,6 @@
 grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
 
+
 def print_grades(grades_input):
     for grade in grades_input:
         print grade
@@ -13,12 +14,17 @@ def grades_sum(scores):
     for score in scores:
         total += score
     return total
+
+
 print grades_sum(grades)
+
 
 def grades_average(grades_input):
     sum_of_grades = grades_sum(grades_input)
-    average = sum_of_grades /float(len(grades_input))
+    average = sum_of_grades / float(len(grades_input))
     return average
+
+
 print grades_average(grades)
 
 
